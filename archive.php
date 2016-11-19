2ae44e0f5413ce6cfaa4c244629c36298b80c512
Bien Angelo Relucano <bien.angelo.relucano@gmail.com>
2016-11-19 22:53:14 +0800

    Archive and Read More tag

--- archive.php
+++ archive.php
@@ -1,5 +1,29 @@
 <?php get_header();
-    if (have_posts()) : while (have_posts()) : the_post(); ?>
+    if (have_posts()) : ?>
+
+    <h2>
+    <?php
+        if (is_category()) {
+            single_cat_title();
+        } elseif (is_tag()) {
+            single_tag_title();
+        } elseif (is_author()) {
+            the_post();
+            echo "Author Archives: " . get_the_author();
+            rewind_posts();
+        } elseif (is_day()) {
+            echo "Daily Archives: " . get_the_date();
+        } elseif (is_month()) {
+            echo "Monthly  Archives: " . get_the_date("F Y");
+        } elseif (is_year()) {
+            echo "Yearly Archives: " . get_the_date("Y");
+        } else {
+            echo "Archive";
+        }
+    ?>
+    </h2>
+
+    <?php while (have_posts()) : the_post(); ?>
         <article class="post">
             <a href="<?php the_permalink(); ?>"><h4><?php the_title(); ?></h4></a>
             <p class="post-info">
@@ -17,7 +41,7 @@
                     echo trim($output, $separator);
                 ?>
             </p>
-            <?php the_content("Continue reading &raquo;"); ?>
+            <?php the_excerpt(); ?>
         </article>
 <?php endwhile; else : ?>
     <h4>No content found</h4>
