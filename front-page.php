2727031a5dd1132979d2d3f62e418620d1bfd48e
Bien Angelo Relucano <bien.angelo.relucano@gmail.com>
2016-12-11 00:20:51 +0800

    Custom WP_Query loop

--- front-page.php
+++ front-page.php
@@ -9,7 +9,17 @@
         else :
     ?>
         <h4>No content found</h4>
-    <?php endif; ?>
+    <?php endif;
+        $lipsumCatId = get_category_by_slug("lipsum")->term_id;
+        $lipsumPosts = new WP_Query("cat=" . $lipsumCatId . "&posts_per_page=2&orderby=title&order=DESC");
+        if ($lipsumPosts->have_posts()) :
+            while ($lipsumPosts->have_posts()) : $lipsumPosts->the_post(); ?>
+                <h2><?php the_title(); ?></h2>
+            <?php endwhile; else : ?>
+            <h4>No lipsum posts</h4>
+        <?php endif;
+        wp_reset_postdata();
+    ?>
 </div>
 <?php get_footer();
 ?>
